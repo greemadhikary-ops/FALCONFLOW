97df3ba34be1df368864b2840251f07ec6d4de2a
greemadhikary-ops <greemadhikary@gmail.com>
2025-12-07 20:35:44 +0530

    Update configuration values in config.js

--- config.js
+++ config.js
@@ -1,27 +1,37 @@
-
-
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "mongodb+srv://tarunadhikary20:admayukhkary@discordbot.e0fxrmf.mongodb.net/?appName=DiscordBot",
-  spotifyClientId : "",
-  spotifyClientSecret : "",
-  setupFilePath: './commands/setup.json',
-  commandsDir: './commands',  
+  ownerID: ["1004206704994566164"],
+
+  mongodbUri: "mongodb+srv://tanjirokamado:admayukhkary@discordbot.4edrfuu.mongodb.net/?appName=DiscordBot",
+
+  spotifyClientId: "d92baed9605a45a39ed7c2a2d960b1c1",
+  spotifyClientSecret: "e9b29f6739de4315bc03b6d8a8e93b03",
+
+  setupFilePath: "./commands/setup.json",
+  commandsDir: "./commands",
+
   embedColor: "#1db954",
-  activityName: "YouTube Music", 
-  activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
+  activityName: "YouTube Music",
+  activityType: "LISTENING",
+
   SupportServer: "https://discord.gg/xQF9f9yUEM",
-  embedTimeout: 5, 
-  errorLog: "", 
+  embedTimeout: 5,
+
+  showProgressBar: false,
+  showVisualizer: false,
+  generateSongCard: true,
+
+  lowMemoryMode: true,
+  errorLog: "",
+
   nodes: [
-     {
-            name: "GlceYT",
-            password: "glace",
-            host: "us-01.strixnodes.com",
-            port: 8003,
-            secure: false
+    {
+      name: "Harmonix-NODE2",
+      password: "Kaun.Yuvraj",
+      host: "zac.hidencloud.com",
+      port: 24627,
+      secure: false
     }
   ]
-}
+};
